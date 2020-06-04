c6d95ffdd71accab6c657cd7112794723f544022
freddiewarren <fredswow@live.co.uk>
2020-06-04 14:17:53 +0100

    footer - added container so items are consitent with contents of page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,15 +4,15 @@ const Footer = () => {
 
   return (
     <footer>
+      <div className="container">
+         <h4>TechWorld</h4>
 
-      <div className="footer-title">
-       <h4>TechWorld</h4>
+
+        <ul>
+          <li><a href="#"><i className="fab fa-twitter-square"></i></a></li>
+          <li><a href="#"><i className="fab fa-facebook-square"></i></a></li>
+        </ul>
       </div>
-
-      <ul>
-        <li><a href="#"><i className="fab fa-twitter-square"></i></a></li>
-        <li><a href="#"><i className="fab fa-facebook-square"></i></a></li>
-      </ul>
     </footer>
   )
 }
